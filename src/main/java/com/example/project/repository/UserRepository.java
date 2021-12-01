51f39aba0a680aabe9dda1f03ea6acd8a853c16a
wogns1026 <sksmsljh1026@gmail.com>
2021-12-01 23:56:17 +0900

    feat: User 테이블 생성 - User 테이블을 위한 Entity, Repository 생성 - hibernate ddl-auto update로 설정

---
+++ src/main/java/com/example/project/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.project.repository;
+
+import com.example.project.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
