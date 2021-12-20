0ebb811ce8ef08fb8bc751c44a128a712bd4748c
wogns1026 <sksmsljh1026@gmail.com>
2021-12-20 23:11:39 +0900

    fix: cors 에러 해결 - 우선 CrossOrigin 어노테이션을 Controller에 선언해줌으로써 해결 - 추후 CorsFilter를 생성해서 Controller마다 선언할 필요 없도록 개선필요

--- src/main/java/com/example/project/controller/ItemController.java
+++ src/main/java/com/example/project/controller/ItemController.java
@@ -7,10 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +16,7 @@ import java.util.Map;
 @RestController
 @RequestMapping(value = "/api/v1")
 @RequiredArgsConstructor
+@CrossOrigin
 public class ItemController {
 
     private final ItemService itemService;
