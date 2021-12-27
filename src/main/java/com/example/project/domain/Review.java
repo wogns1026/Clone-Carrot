4df6b6b35ce9de689cdaf8873442c2b499bd417e
wogns1026 <sksmsljh1026@gmail.com>
2021-12-28 01:03:31 +0900

    feat: 엔티티 생성 - 게시판(동네정보)에 이용될 게시판, 리뷰 엔티티 생성

---
+++ src/main/java/com/example/project/domain/Review.java
@@ -0,0 +1,25 @@
+package com.example.project.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "TB_REVIEWS")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long reviewId;
+
+    private Long boardId;
+
+    private String content;
+
+    private Long userId;
+
+    private LocalDateTime regTime;
+
+    private Long parentReviewId;
+}
