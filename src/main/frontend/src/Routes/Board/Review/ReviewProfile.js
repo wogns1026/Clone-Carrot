d3babae199a6b2531e00a870ec3c5ec5ecef3835
jihoon <hun2ya9@gmail.com>
2021-12-29 00:49:15 +0900

    feat(Review): 댓글 컴포넌트
    Review 의 새 기능:
    - reviewID와 parentReviewID를 비교하여 댓글, 대댓글을 표현함
    - 대댓글은 들여쓰기가 됨

---
+++ src/main/frontend/src/Routes/Board/Review/ReviewProfile.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  FlexBox,
+  LazyBackgroundImage,
+  LinkTo,
+  Text,
+} from "Components/Universal";
+import theme from "Style/theme";
+
+const ReviewProfile = ({ id, src, name, location }) => {
+  return (
+    <LinkTo to={{ pathname: `/profile/${id}` }}>
+      <FlexBox flexAlign="center" gap="10px" marginBottom="10px">
+        <LazyBackgroundImage
+          src={src}
+          size="24px"
+          round
+          center
+          border={theme.colors.lightDark}
+          alt=""
+        />
+        <Text fontSize="15px" fontWeight={700}>
+          {name}
+        </Text>
+        <Text fontSize="13px">{location}</Text>
+      </FlexBox>
+    </LinkTo>
+  );
+};
+
+export default ReviewProfile;
