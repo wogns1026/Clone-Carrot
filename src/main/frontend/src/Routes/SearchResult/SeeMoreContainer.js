1a8202a0e9a4579b958eaec8628b8709dcfe3af4
jihoon <hun2ya9@gmail.com>
2021-12-28 21:13:09 +0900

    feat(SearchResult, SeeMoreContainer): 검색 결과 페이지
    SearchResult 의 새 기능:
    - 검색 결과 (중고 매물, 게시판)를 각각 리스트 형태로 보여줌
    
    SeeMoreContainer 의 새 기능:
    - 더보기 버튼을 누르면 데이터를 추가로 로드하는 컴포넌트
    - 실제로 추가 데이터 불러오는 로직은 아직 존재하지 않음

---
+++ src/main/frontend/src/Routes/SearchResult/SeeMoreContainer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Box, Text, Button, HorizontalDivider } from "Components/Universal";
+import theme from "Style/theme";
+
+const SeeMoreContianer = ({ title, bgColor, onClicked, children }) => {
+  return (
+    <Box
+      fullSize
+      bgColor={bgColor}
+      border={theme.colors.grey}
+      borderRadius="10px"
+    >
+      <Box horizontalMargin="40px">
+        <Box verticalMargin="23px">
+          <Text fontSize="18px" fontWeight={800}>
+            {title}
+          </Text>
+        </Box>
+        {children}
+      </Box>
+      <HorizontalDivider />
+      <Button fullSize onClick={onClicked} height="50px">
+        <Text fontSize="16px" opacity={0.6}>
+          더보기
+        </Text>
+      </Button>
+    </Box>
+  );
+};
+export default SeeMoreContianer;
