62c92040a84fdf464043fc66eadd2c638c02515e
wogns1026 <sksmsljh1026@gmail.com>
2022-01-17 00:35:32 +0900

    feat: 게시글 저장할 시 게시글 id 리턴하도록 수정

--- src/main/java/com/example/project/controller/BoardController.java
+++ src/main/java/com/example/project/controller/BoardController.java
@@ -48,6 +48,7 @@ public class BoardController {
         boardService.saveBoard(board);
 
         Map<String, Object> result = new HashMap<>();
+        result.put("boardId", board.getBoardId());
         result.put("success", "success");
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
